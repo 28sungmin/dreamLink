90631fdd883c643c3de42cbf379b9abec4b719da
28sungmin <gooddays4001@naver.com>
2025-03-18 16:48:47 +0900

    aws 용 코드 추가

--- src/main/java/com/dreamlink/DreamlinkApplication.java
+++ src/main/java/com/dreamlink/DreamlinkApplication.java
@@ -2,9 +2,10 @@ package com.dreamlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DreamlinkApplication {
+public class DreamlinkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DreamlinkApplication.class, args);
